5ea0365393783d0107ece6c664ba749ddeeda609
kajalsharma127265-dotcom <kajal.sharma127265@marwadiuniversity.ac.in>
2026-03-02 10:05:36 +0530

    Add files via upload

---
+++ lab11 max.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = new int[5];
+
+        Console.WriteLine("Enter 5 numbers:");
+
+        // Input 5 numbers
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Number " + (i + 1) + ": ");
+            numbers[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        // Assume first number is minimum
+        int min = numbers[0];
+
+        // Find minimum
+        for (int i = 1; i < 5; i++)
+        {
+            if (numbers[i] < min)
+            {
+                min = numbers[i];
+            }
+        }
+
+        Console.WriteLine("Minimum number is: " + min);
+    }
+}
---
+++ lab11min.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = new int[5];
+
+        Console.WriteLine("Enter 5 numbers:");
+
+        // Input 5 numbers
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Number " + (i + 1) + ": ");
+            numbers[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        // Assume first number is maximum
+        int max = numbers[0];
+
+        // Find maximum
+        for (int i = 1; i < 5; i++)
+        {
+            if (numbers[i] > max)
+            {
+                max = numbers[i];
+            }
+        }
+
+        Console.WriteLine("Maximum number is: " + max);
+    }
+}
