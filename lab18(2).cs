40949d937ef83808c883e4e86550d3f8a3812510
kajalsharma127265-dotcom <kajal.sharma127265@marwadiuniversity.ac.in>
2026-04-07 08:14:54 +0530

    Add files via upload

---
+++ lab18(2).cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class TryCatchFinallyDemo
+{
+    static void Main()
+    {
+        int a, b, result;
+
+        try
+        {
+            Console.Write("Enter first number: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            result = a / b;   // This may cause an exception if b = 0
+
+            Console.WriteLine("Result = " + result);
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Error: Cannot divide by zero.");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Error: Please enter valid numbers.");
+        }
+        finally
+        {
+            Console.WriteLine("Program execution completed.");
+        }
+
+        Console.ReadKey();
+    }
+}
